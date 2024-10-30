22bf6bc32e56d1d706bc4eccf93d709d209c1d80
Akaren2022 <karenaitana21@gmail.com>
2024-10-29 23:05:49 -0500

    Creamos el Dockerfile con los matinifiestos y requerimientos

--- python/src/gateway/server.py
+++ python/src/gateway/server.py
@@ -3,7 +3,7 @@ from flask import Flask, request, send_file
 from flask_pymongo import PyMongo
 from auth import validate
 from auth_svc import access
-# from storage import util
+from storage import util
 from bson.objectid import ObjectId
 
 server = Flask(__name__)
